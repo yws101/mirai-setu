5d5c6d162af1647f701029bb14c1ed97a8e6882a
bloody-rabbit <2275072700@qq.com>
2021-04-06 21:21:53 +0800

    增加权限模式控制

--- src/main/kotlin/cn/blrabbit/mirai/utils/Power.kt
+++ src/main/kotlin/cn/blrabbit/mirai/utils/Power.kt
@@ -6,7 +6,17 @@ import net.mamoe.mirai.contact.isOperator
 
 // 权限判断（获取以后会搞上更好的方法？）
 fun checkpower(sender: Member): Boolean {
-    if (MySetting.masterid.contains(sender.id))
-        return true
-    return sender.isOperator()
+    when (MySetting.mastermode) {
+        1 -> {
+            if (MySetting.masterid.contains(sender.id))
+                return true
+        }
+        2 -> {
+            if (MySetting.masterid.contains(sender.id))
+                return true
+            return sender.isOperator()
+        }
+        else -> return false
+    }
+    return false
 }
