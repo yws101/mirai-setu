084fa4da2c6c082acdfca32c7cb0643f9974913f
bloody-rabbit <2275072700@qq.com>
2021-04-15 10:30:58 +0800

    增加FantasyZone的Json解析

---
+++ src/main/kotlin/cn/blrabbit/mirai/fantasyzone/jsondata/FantasyZoneJson.kt
@@ -0,0 +1,16 @@
+package cn.blrabbit.mirai.fantasyzone.jsondata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FantasyZoneJson(
+    val description: String?,
+    val height: String?,
+    val id: String?,
+    val tags: List<String>?,
+    val title: String?,
+    val url: String?,
+    val userId: String?,
+    val userName: String?,
+    val width: String?
+)
