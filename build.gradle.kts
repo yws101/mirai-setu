7aa4d1897fa35ec963d9c9a60c6d920db6e7126e
bloody-rabbit <2275072700@qq.com>
2021-04-29 10:08:48 +0800

    更新Mirai到2.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     val kotlinVersion = "1.4.30"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.5.1" // mirai-console version
+    id("net.mamoe.mirai-console") version "2.6.0" // mirai-console version
 }
 
 mirai {
-    coreVersion = "2.5.1" // mirai-core version
+    coreVersion = "2.6.2" // mirai-core version
 }
 
 group = "cn.blrabbit"
