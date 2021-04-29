e2be250a54258c9512b69f105d9da3a8299edbe1
bloody-rabbit <2275072700@qq.com>
2021-04-29 10:09:17 +0800

    增加色图接口

--- src/main/kotlin/cn/blrabbit/mirai/fantasyzone/jsondata/FantasyZoneJson.kt
+++ src/main/kotlin/cn/blrabbit/mirai/fantasyzone/jsondata/FantasyZoneJson.kt
@@ -4,13 +4,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class FantasyZoneJson(
-    val description: String?,
-    val height: String?,
-    val id: String?,
-    val tags: List<String>?,
-    val title: String?,
-    val url: String?,
-    val userId: String?,
-    val userName: String?,
-    val width: String?
+    val description: String,
+    val height: String,
+    val id: String,
+    val tags: List<String>,
+    val title: String,
+    val url: String,
+    val userId: String,
+    val userName: String,
+    val width: String
 )
---
+++ src/main/kotlin/cn/blrabbit/mirai/setu/Setu.kt
@@ -0,0 +1,7 @@
+package cn.blrabbit.mirai.setu
+
+interface Setu {
+    fun getsetu()
+    fun getsetu(keyword: String)
+    fun sendmessage()
+}
