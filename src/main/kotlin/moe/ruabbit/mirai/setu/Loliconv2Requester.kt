d8a305e619bf223114f8b99ac4b3e3a6320eae13
bloody-rabbit <2275072700@qq.com>
2021-08-18 00:06:30 +0800

    添加完整的setubody，为群友提出的SetuQL预备

--- src/main/kotlin/moe/ruabbit/mirai/setu/Loliconv2Requester.kt
+++ src/main/kotlin/moe/ruabbit/mirai/setu/Loliconv2Requester.kt
@@ -33,17 +33,29 @@ class Loliconv2Requester(val subject: Contact) : Setu(subject) {
 
     /**
      *  构造post请求的body信息，注意这个body并不是完整的body，详细的请求信息请参考官网的配置信息
-     *  @param r18 是否获取R18模式(0.普通模式 1.R18模式 2.混合模式)
-     *  @param num 获取图片的数量
-     *  @param keyword 关键词搜索，单个词搜索题目，作者，以及tag
-     *  @param tag 多tag搜索的多个tag
+     *  @param r18 0为非 R18，1为 R18，2为混合（在库中的分类，不等同于作品本身的 R18 标识）
+     *  @param num 一次返回的结果数量，范围为1到100；在指定关键字或标签的情况下，结果数量可能会不足指定的数量
+     *  @param uid 返回指定uid作者的作品，最多20个
+     *  @param keyword 返回从标题、作者、标签中按指定关键字模糊匹配的结果，大小写不敏感，性能和准度较差且功能单一，建议使用tag代替
+     *  @param tag 返回匹配指定标签的作品
+     *  @param size 返回指定图片规格的地址
+     *  @param proxy 设置图片地址所使用的在线反代服务
+     *  @param dateAfter 返回在这个时间及以后上传的作品；时间戳，单位为毫秒
+     *  @param dateBefore 返回在这个时间及以前上传的作品；时间戳，单位为毫秒
+     *  @param dsc 设置为任意真值以禁用对某些缩写keyword和tag的自动转换
      */
     @Serializable
     data class LoliconRequest(
-        val r18: Int,
-        val num: Int,
+        val r18: Int = 0,
+        val num: Int = 1,
+        val uid: Int? = null,
         val keyword: String? = null,
-        val tag: List<String>? = null
+        val tag: List<String>? = null,
+        val size: List<String> = listOf("original"),
+        val proxy: String = "i.pixiv.cat",
+        val dateAfter: Int? = null,
+        val dateBefore: Int? = null,
+        val dsc: Boolean = false
     )
 
     /**
@@ -54,7 +66,7 @@ class Loliconv2Requester(val subject: Contact) : Setu(subject) {
     override suspend fun requestSetu(num: Int, r18: Int) {
         // TODO("设置请求的数量上限")
         // 构造post请求的body
-        val requsterbody = LoliconRequest(SetuData.groupPolicy[subject.id]!!, num)
+        val requsterbody = LoliconRequest(r18 = r18, num = num)
 
         parseresponse(requsterbody)
     }
@@ -68,9 +80,9 @@ class Loliconv2Requester(val subject: Contact) : Setu(subject) {
     override suspend fun requestSetu(tags: List<String>, num: Int, r18: Int) {
         val requsterbody: LoliconRequest =
             if (tags.size == 1)
-                LoliconRequest(r18, num, keyword = tags[0])
+                LoliconRequest(r18 = r18, num = num, keyword = tags[0])
             else
-                LoliconRequest(r18, num, tag = tags)
+                LoliconRequest(r18 = r18, num = num, tag = tags)
 
         parseresponse(requsterbody)
 
